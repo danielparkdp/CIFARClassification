a88132b93d96679b974fe123b98a30c9bbff88b1
Daniel Park <danielpark779@gmail.com>
2019-11-12 23:02:09 -0500

    preprocess complete

--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,9 @@ def unpickle(file):
 	:param file: the file with data
 	:return: dictionary of data
 	"""
-	pass
+	with open(file, 'rb') as fo:
+		dict = pickle.load(fo, encoding='bytes')
+	return dict
 
 
 def get_data(file_path, first_class, second_class):
@@ -21,4 +23,25 @@ def get_data(file_path, first_class, second_class):
 	:param first_class:  an integer (0-9) representing the second target
 	:return: inputs and tensor of the given labels
 	"""
-	pass
+	unpickled_file = unpickle(file_path)
+	inputs = unpickled_file[b'data']
+	labels = unpickled_file[b'labels']
+
+	inpl = []
+	labl = []
+
+	for i in range(0,len(inputs)):
+		if labels[i] == first_class:
+			inpl.append(inputs[i])
+			labl.append(0)
+		elif labels[i] == second_class:
+			inpl.append(inputs[i])
+			labl.append(1)
+
+	inp = np.array(inpl)
+	lab = np.array(labl)
+	inp = np.reshape(inp, (-1, 3, 32 ,32))
+	inp = inp.transpose((0,2,3,1))
+	lab = tf.one_hot(lab, depth=2)
+	inp = tf.cast(inp, tf.float32)
+	return inp, lab
