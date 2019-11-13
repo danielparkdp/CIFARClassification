f871a6f404da8aa1d8bee4de8faaaabf432d5f46
Daniel Park <danielpark779@gmail.com>
2019-11-13 11:36:31 -0500

    structure

---
+++ model.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from matplotlib import pyplot as plt
+from preprocess import get_data
+from convolution import conv2d
+import os
+import tensorflow as tf
+import numpy as np
+import random
+
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
