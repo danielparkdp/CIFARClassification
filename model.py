3cde6542eec492327835c67ae10ece0d5a184b56
Daniel Park <danielpark779@gmail.com>
2019-11-13 11:40:18 -0500

    model functions

--- model.py
+++ model.py
@@ -1,11 +1,32 @@
 from __future__ import absolute_import
-from matplotlib import pyplot as plt
 from preprocess import get_data
 from convolution import conv2d
 import os
 import tensorflow as tf
 import numpy as np
 import random
+
+class Model(tf.keras.Model):
+	def __init__(self):
+		pass
+
+
+	def call(self, inputs, is_testing=False):
+		pass
+
+	def loss(self, logits, labels):
+		pass
+
+	def accuracy(self, logits, labels):
+		pass
+
+
+def train(model, train_inputs, train_labels):
+	pass
+
+def test(model, test_inputs, test_labels):
+	pass
+
 
 
 
